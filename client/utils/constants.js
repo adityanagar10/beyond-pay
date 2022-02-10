b2902dcf02ea5f69a603b8a33a5aa6b96129d80a
Aditya Nagar <nagar.s.aditya@gmail.com>
2022-02-10 16:56:02 +0530

    Smart Contract deployed

---
+++ client/utils/constants.js
@@ -0,0 +1,5 @@
+import abi from './Transactions.json'
+
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x4694C2BEFCc5c368aE30CDb2B77A85d6b8F73ff4' ;
--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -1,29 +1,17 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
-
 const main = async () => {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  // This is a Function Factory which generates instances of the contract
 
-  await greeter.deployed();
+  const Transactions = await hre.ethers.getContractFactory("Transactions");
 
-  console.log("Greeter deployed to:", greeter.address);
+  // A single instance of the contract
+
+  const transactions = await Transactions.deploy();
+
+  await transactions.deployed();
+
+  console.log("Transactions deployed to:", transactions.address);
 }
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 
 const runMain = async () => {
   try{
