8afd64dda9c18ea31ef74c26b42059f1b53f7e3a
Aditya Nagar <nagar.s.aditya@gmail.com>
2022-03-02 21:42:39 +0530

    Transaction added

--- client/utils/constants.js
+++ client/utils/constants.js
@@ -2,4 +2,4 @@ import abi from './Transactions.json'
 
 
 export const contractABI = abi.abi;
-export const contractAddress = '0x4694C2BEFCc5c368aE30CDb2B77A85d6b8F73ff4' ;
+export const contractAddress = '0x4694C2BEFCc5c368aE30CDb2B77A85d6b8F73ff4';
